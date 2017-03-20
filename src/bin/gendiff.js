3d1ebc7080cfe14773da06d33fa8272fe7f8454d
Grigori Grudinin <grgr14@mail.ru>
2017-03-20 21:52:23 +0300

    update gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,14 +4,8 @@ const program = require('commander');
 
 program
   .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-  .parse(process.argv);
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<first_config> <second_config>');
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+program.parse(process.argv);
